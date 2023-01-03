ffc4cc51e69e07de9041f1a78be7baffec7e3c85
DarkStar☀ <61005029+Twin-Flame@users.noreply.github.com>
2023-01-03 14:16:47 -0600

    Update Merge and convert.sh

--- Merge and convert.sh
+++ Merge and convert.sh
@@ -3,3 +3,5 @@ for i in `\ls *.ts | sort -V`; do echo "file '$i'"; done >> mylist.txt;
 ffmpeg -f concat -safe "0" -i mylist.txt -c copy merge.ts
 
 ffmpeg -i merge.ts -map 0 -c copy output.mp4
+
+#source: https://stackoverflow.com/questions/22188332/download-content-video-from-video-stream-with-a-path-of-ts-or-m3u8-file-throug
